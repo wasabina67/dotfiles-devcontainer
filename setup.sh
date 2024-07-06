dbb8017bb825c888eae925dd7247d6be829a43b3
wasabina67 <wasabina67@gmail.com>
2024-07-06 20:44:43 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,11 @@ apt-get update && \
         less \
         vim \
         wget
+
+if [ -f ~/.bash_aliases ]; then
+    TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+    BK=~/.bash_aliases.${TIMESTAMP}
+    mv ~/.bash_aliases ${BK}
+fi
+
+wget https://raw.githubusercontent.com/wasabina67/dotfiles-devcontainer/main/.bash_aliases -O ~/.bash_aliases
