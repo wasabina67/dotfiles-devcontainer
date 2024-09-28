0432a5d6aa71e872cd3ee624b249e6ebe79baf3a
wasabina67 <wasabina67@gmail.com>
2024-09-28 17:25:54 +0900

    Create alpine/setup.sh

---
+++ alpine/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get update && \
+    apt-get install -y \
+        bash-completion \
+        git \
+        less \
+        vim \
+        wget
+
+if [ -f ~/.bash_aliases ]; then
+    TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+    BK=~/.bash_aliases.${TIMESTAMP}
+    mv ~/.bash_aliases ${BK}
+fi
+
+wget https://raw.githubusercontent.com/wasabina67/dotfiles-devcontainer/main/.bash_aliases -O ~/.bash_aliases
